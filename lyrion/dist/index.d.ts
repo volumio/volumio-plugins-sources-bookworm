5baee3b21a14a8b5dccf327c929f1c5684d84335
patrickkfkan <patrickkfkan@gmail.com>
2024-11-14 19:11:58 +0800

    Add Lyrion Music Server plugin v1.0.0

---
+++ lyrion/dist/index.d.ts
@@ -0,0 +1,10 @@
+declare class ControllerLyrion {
+    #private;
+    constructor(context: any);
+    getUIConfig(): any;
+    onVolumioStart(): any;
+    onStart(): any;
+    onStop(): any;
+}
+export = ControllerLyrion;
+//# sourceMappingURL=index.d.ts.map
