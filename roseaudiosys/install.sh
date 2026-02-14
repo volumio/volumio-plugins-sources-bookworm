557fff6af5ad30832995454b8fd6ae9479c42ea0
lelsteph <slelong95@gmail.com>
2026-02-14 18:41:43 +0100

    Updates in install.sh as recommended by Volumio team

--- roseaudiosys/install.sh
+++ roseaudiosys/install.sh
@@ -3,7 +3,7 @@
 echo "Installing roseaudiosys Dependencies"
 sudo apt-get update
 # Install the required packages via apt-get
-sudo apt-get -y install
+# sudo apt-get -y install
 
 # If you need to differentiate install for armhf and i386 you can get the variable like this
 #DPKG_ARCH=`dpkg --print-architecture`
