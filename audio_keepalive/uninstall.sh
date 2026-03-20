76f3e66d642f3501e8a4c1a28293cb802d523b06
foonerd <andrew@dt-ltd.net>
2026-03-20 08:51:42 +0000

    Audio Keepalive - timerfd-paced ioplug with -100dB noise keepalive

--- audio_keepalive/uninstall.sh
+++ audio_keepalive/uninstall.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 echo "Uninstalling Audio Keepalive"
+
+for dir in /usr/lib/arm-linux-gnueabihf/alsa-lib /usr/lib/x86_64-linux-gnu/alsa-lib; do
+    if [ -f "${dir}/libasound_module_pcm_keepalive.so" ]; then
+        sudo rm -f "${dir}/libasound_module_pcm_keepalive.so"
+        echo "Removed ${dir}/libasound_module_pcm_keepalive.so"
+    fi
+done
+
 echo "Done"
 echo "pluginuninstallend"
